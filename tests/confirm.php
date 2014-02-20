4f1a82a78ccf32b99d4ef51fd3ab4e360cb19396
Jesse Clark <jesse.clark@unco.edu>
2014-02-20 12:29:34 -0700

    Get confirm to work.

--- tests/confirm.php
+++ tests/confirm.php
@@ -1,3 +1,8 @@
+<?php
+	session_start();
+	error_reporting(E_ALL);
+	ini_set('display_errors', '1');
+?>
 <!doctype html>
 <html>
 <head>
@@ -13,37 +18,57 @@ include("../config.php");
 mysql_select_db($database, $connection);
 
 include("../classes/scoresheet.php");
-include("../classes/scoresheetTest.php");
+include("classes/scoresheetTest.php");
 
-if (is_numeric($_REQUEST["scoresheetID"])){
+if (isset($_REQUEST["scoresheetID"])){
 
-	$ConfirmScoreSheet = new Scoresheet();
-	$ConfirmScoreSheet->GetJudgingNumberByBrewID($_REQUEST["scoresheetID"]);
+	$ConfirmScoreSheet = new ScoreSheetTest();	
+	$ConfirmScoreSheet->BrewID = $_REQUEST["scoresheetID"];
+	$ConfirmScoreSheet->CheckConfirm();
 
 ?>
 
 <form id="form1" name="form1" method="post" action="confirm.php">
 	<h2>Scoresheet: <?php echo $ConfirmScoreSheet->ReturnJudgingNumber(); ?></h2>
-	<p>ConfirmID: <?php echo $ConfirmScoreSheet->ConfirmID; ?></p>
-	<p>Scoresheet ID: <?php echo $ConfirmScoreSheet->JudgingNumber; ?><input type="hidden" name="JudgingID" value="<?php echo $ConfirmScoreSheet->JudgingNumber; ?>"></p>
+	<p>ConfirmID: <?php echo $ConfirmScoreSheet->ConfirmID; ?><input type="hidden" name="ConfirmID" value="<?php echo $ConfirmScoreSheet->ConfirmID; ?>"></p>
 	<p>Judging ID: <?php echo $ConfirmScoreSheet->BrewID; ?><input type="hidden" name="BrewID" value="<?php echo $ConfirmScoreSheet->BrewID; ?>"></p>
 	<p>Scoresheet Status: 
-	<select name="status">
+	<select name="Status">
 		<option value="0">Error</option>
-		<option value="1">Confirmed.</option>
+		<option value="1" <?php if ($ConfirmScoreSheet->Status == "1"){ ?>selected="selected"<?php } ?>>Confirmed.</option>
 	</select>
 	</p>
-	<p>Reviewed by: <input type="text" name="VerifiedBy" value=""/></p>
-	<p>Comments:<textarea name="comments"> </textarea></p>
+	<p>Reviewed by: <input type="text" name="VerifiedBy" value="<?php echo $ConfirmScoreSheet->VerifiedBy; ?>"/></p>
+	<p>Comments:<textarea name="Comments"><?php echo $ConfirmScoreSheet->Comments; ?></textarea></p>
 	<p><input type="submit"></p>
 </form>
+
+<?php } ?>
 
 
 <?php
 
+if (isset($_POST["ConfirmID"])){
+
+	include("../../../site/config.php");
+	mysql_select_db($database, $connection);
+
+	if ($_POST["ConfirmID"] == ""){
+		$sql = "INSERT INTO `downloadConfirm` (`ScoresheetID`, `Status`, `VerifiedBy`, `Comments`) VALUES ('" . $_POST["BrewID"] . "', '" . $_POST["Status"] . "', '" . $_POST["VerifiedBy"] . "', '" . $_POST["Comments"] . "';";
+	}else{
+		//update	
+		$sql = "UPDATE `downloadConfirm` 	
+				SET `Status` = '" . $_POST["Status"] . "', 
+				`VerifiedBy` = '" . $_POST["VerifiedBy"] . "', 
+				`Comments` = '" . $_POST["Comments"] . "' 
+				WHERE `ID` = '" . $_POST["ConfirmID"] . "';";
+	}
+
+		$result = mysql_query($sql) or die('Query failed (scoresheet.php): ' . mysql_error());
+
+
+		echo "<h2>Comfirmed Saved.</h2>";
 }
-
-
 ?>
 
 </body>
