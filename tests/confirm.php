efa96c72b99f2eb805aa41bdda0d15f00d7a6554
Jesse Clark <jesse.clark@unco.edu>
2014-02-20 10:51:37 -0700

    Create form.

--- tests/confirm.php
+++ tests/confirm.php
@@ -19,13 +19,23 @@ if (is_numeric($_REQUEST["scoresheetID"])){
 
 	$ConfirmScoreSheet = new Scoresheet();
 	$ConfirmScoreSheet->GetJudgingNumberByBrewID($_REQUEST["scoresheetID"]);
-	var_dump($ConfirmScoreSheet);
 
 ?>
 
-<form id="form1" name="form1" method="post" action="">
-
-
+<form id="form1" name="form1" method="post" action="confirm.php">
+	<h2>Scoresheet: <?php echo $ConfirmScoreSheet->ReturnJudgingNumber(); ?></h2>
+	<p>ConfirmID: <?php echo $ConfirmScoreSheet->ConfirmID; ?></p>
+	<p>Scoresheet ID: <?php echo $ConfirmScoreSheet->JudgingNumber; ?><input type="hidden" name="JudgingID" value="<?php echo $ConfirmScoreSheet->JudgingNumber; ?>"></p>
+	<p>Judging ID: <?php echo $ConfirmScoreSheet->BrewID; ?><input type="hidden" name="BrewID" value="<?php echo $ConfirmScoreSheet->BrewID; ?>"></p>
+	<p>Scoresheet Status: 
+	<select name="status">
+		<option value="0">Error</option>
+		<option value="1">Confirmed.</option>
+	</select>
+	</p>
+	<p>Reviewed by: <input type="text" name="VerifiedBy" value=""/></p>
+	<p>Comments:<textarea name="comments"> </textarea></p>
+	<p><input type="submit"></p>
 </form>
 
 
